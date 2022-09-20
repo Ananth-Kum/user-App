97ea5d6207372b96f9b4d00460d52d1a4a6debfe
Ananth-Kum <ananth.kumar@synoriq.in>
2022-09-20 16:02:06 +0530

    cors fixes

--- server.js
+++ server.js
@@ -8,24 +8,7 @@ require("./config/db");
 
 // create express app
 const app = express();
-const whitelist = ["http://localhost:3000"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) {
-      return callback(null, true);
-    }
-
-    if (whitelist.indexOf(origin) === -1) {
-      var msg =
-        "The CORS policy for this site does not " +
-        "allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // define port to run express app
 const port = process.env.PORT || 3000;
