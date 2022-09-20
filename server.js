5a08757fbdc6ce6f063446a56804e43cba0b90b5
Ananth-Kum <ananth.kumar@synoriq.in>
2022-09-20 17:01:34 +0530

    fixed cors issues

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // require express and body parser
 const express = require("express");
 const bodyParser = require("body-parser");
-// const cors = require("cors");
+const cors = require("cors");
 
 // Import DB Connection
 require("./config/db");
 
 // create express app
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 // define port to run express app
 const port = process.env.PORT || 3000;
@@ -34,15 +34,15 @@ app.listen(port, () => {
 var routes = require("./api/routes/userRoutes"); //importing route
 routes(app);
 
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  next();
-});
+// app.use(function (req, res, next) {
+//   //Enabling CORS
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Private-Network", true);
+//   next();
+// });
